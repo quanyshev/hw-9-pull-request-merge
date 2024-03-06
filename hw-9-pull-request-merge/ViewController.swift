7461a8c4aa5fd4cd5f320b316a1b17ce8c4e7cc4
quanyshev <kuanyshev.asylbek@gmail.com>
2024-03-06 16:59:08 +0500

    Initial Commit

---
+++ hw-9-pull-request-merge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw-9-pull-request-merge
+//
+//  Created by Bek Q on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
